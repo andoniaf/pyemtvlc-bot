b32c399092d65f18900e381e944b09aa5ae72f91
Andoni A <andonialonsof@gmail.com>
2019-03-19 22:29:26 +0100

    WIP

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@
 # Librerías
 import telegram
 import logging
+import sys
+from threading import Thread
+from telegram.ext import Updater, CommandHandler, Filters
 
 import time
 from datetime import datetime
@@ -23,9 +26,39 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
+
+def main():
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+
+    # Add your other handlers here...
+
+    def stop_and_restart():
+        """Gracefully stop the Updater and replace the current process with a new one"""
+        updater.stop()
+        os.execl(sys.executable, sys.executable, *sys.argv)
+
+    def restart(update, context):
+        update.message.reply_text('Bot is restarting...')
+        Thread(target=stop_and_restart).start()
+
+    # ...or here...
+
+    dp.add_handler(CommandHandler('r', restart, filters=Filters.user(username='@xTrece')))
+
+    # ...or here, depending on your preference :)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
+
+
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
-def start(bot, update):
-    logger.info("Bot Started")
-    logger.info("Keys: %s" % KEYS)
-    update.message.reply_text(WELCOME_MESSAGE)
+#def start(bot, update):
+#    logger.info("Bot Started")
+#    logger.info("Keys: %s" % KEYS)
+#    update.message.reply_text(WELCOME_MESSAGE)
