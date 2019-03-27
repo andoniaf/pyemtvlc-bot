863184e257aa8afd73e177e1a9e661feff61232d
Andoni A <andonialonsof@gmail.com>
2019-03-27 20:46:14 +0100

    Rename module

--- modules/utils.py
+++ modules/utils.py
@@ -16,30 +16,19 @@ def memory_usage_ps():
 
 
 # Funcion para construir el mensaje que devolverá
-def uptime_string(startup_time_in_seconds, last_error_time):
+def uptime_string():
     # Machine info
     uname = platform.uname()
     uptime_seconds = uptime.uptime()
     # Delta uptime in human readable format
     uptime_string = str(timedelta(seconds=uptime_seconds))
-    # Time now
-    now = time.time()
-    delta = now - startup_time_in_seconds
-    bot_uptime = str(timedelta(seconds=int(delta)))
     # Get memory usage with ps
     memory = memory_usage_ps()
     # Make messsge
     string = ""
     string += "\U0001F4BB Running on " + uname[0] + " " + uname[2] + " " + uname[4] + "\n"
     string += "\U0000231B Uptime: " + uptime_string + "\n"
-    string += "\U0001F916 Bot uptime: " + bot_uptime + "\n"
     string += "\U0001F4CA Bot memory usage: " + memory + "MB"
-
-    if last_error_time is not None:
-        delta = now - last_error_time
-        last_error = str(timedelta(seconds=int(delta)))
-        string += "\n\U0001F480 " + last_error + " without casualties"
-
     return string
 
 
