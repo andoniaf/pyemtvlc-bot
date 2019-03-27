e03a17e187e30e6c12785015b13338b07cf3060f
Andoni A <andonialonsof@gmail.com>
2019-03-27 20:46:56 +0100

    Finished initial refactoring

--- bot.py
+++ bot.py
@@ -8,14 +8,16 @@ import sys
 import os
 from threading import Thread
 from telegram.ext import Updater, CommandHandler, Filters
-from settings import TOKEN, USERS, LOGLEVEL
+from settings import TOKEN, USERS
+
+from modules.utils import uptime_string, query_emt
 
 # Vars
-WELCOME_MSG = 'Bot iniciado y listo para servir!'
+INIT_MSG = 'Bot iniciado y listo para servir!'
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.LOGLEVEL)
+                    level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
 
@@ -25,12 +27,12 @@ def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    # Add your other handlers here...
+    # Functions
     def start(update, context):
-        context.bot.send_message(chat_id=update.message.chat_id, text=WELCOME_MSG)
+        context.bot.send_message(chat_id=update.message.chat_id, text=INIT_MSG)
 
     def stop_and_restart():
-        """Gracefully stop the Updater and replace the current process with a new one"""
+        """Gracefully stop the Updater & replace curr process with a new one"""
         updater.stop()
         os.execl(sys.executable, sys.executable, *sys.argv)
 
@@ -38,9 +40,21 @@ def main():
         update.message.reply_text('Bot is restarting...')
         Thread(target=stop_and_restart).start()
 
-    # Add handlers
+    def uptime(update, context):
+        text = uptime_string()
+        update.message.reply_text(text)
+
+    def emt_info(update, context):
+        msg = update.message.text
+        parada = msg.strip('/emt ')
+        text = query_emt(parada)
+        update.message.reply_text(text)
+
+    # Handlers
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler('r', restart, filters=Filters.user(username='@xTrece')))
+    dp.add_handler(CommandHandler("uptime", uptime))
+    dp.add_handler(CommandHandler("emt", emt_info))
 
     # Start the Bot start_polling() method
     # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
