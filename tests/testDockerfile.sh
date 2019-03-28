ba7c2a4cc0331b32124d86f6404a55df69093e46
Andoni A <andonialonsof@gmail.com>
2019-03-28 20:40:46 +0100

    Ignore DL3018: Unpinned versions

--- tests/testDockerfile.sh
+++ tests/testDockerfile.sh
@@ -2,6 +2,5 @@
 # Based on: https://github.com/pollosp/ruby-docker/blob/master/test.sh
 echo "Lintering..."
 # https://github.com/hadolint/hadolint
-#docker run --rm -i hadolint/hadolint hadolint --ignore DL3018 - < Dockerfile && echo "Lintering OK"
-docker run --rm -i hadolint/hadolint hadolint - < Dockerfile && echo "Lintering OK"
+docker run --rm -i hadolint/hadolint hadolint --ignore DL3018 - < Dockerfile && echo "Lintering OK"
 
