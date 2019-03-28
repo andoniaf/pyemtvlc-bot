685c9029a330f4637e6642e526ed8adbdec9ff05
Andoni A <andonialonsof@gmail.com>
2019-03-28 19:52:14 +0100

    Remove and rename old shit

--- modules/utils.py
+++ modules/utils.py
@@ -6,35 +6,22 @@ import subprocess
 import os
 
 
-# Funcion para medir la memoria usada por el bot
-def memory_usage_ps():
-    #out = subprocess.Popen(['ps', 'v', '-p',
-    out = subprocess.Popen(['ps', '-o', 'pid,user,vsz,rss,comm,args',
-                            str(os.getpid())], stdout=subprocess.PIPE).communicate()[0].split(b'\n')
-    vsz_index = out[0].split().index(b'RSS')
-    mem = float(out[1].split()[vsz_index]) / 1024
-    return "{0:.2f}".format(mem)
-
-
-# Funcion para construir el mensaje que devolverá
+# Build uptime msg
 def uptime_string():
     # Machine info
     uname = platform.uname()
     uptime_seconds = uptime.uptime()
     # Delta uptime in human readable format
     uptime_string = str(timedelta(seconds=uptime_seconds))
-    # Get memory usage with ps
-    #memory = memory_usage_ps()
-    # Make messsge
+    # Build messsge
     string = ""
     string += "\U0001F4BB Running on " + uname[0] + " " + uname[2] + " " + uname[4] + "\n"
     string += "\U0000231B Uptime: " + uptime_string + "\n"
-    #string += "\U0001F4CA Bot memory usage: " + memory + "MB"
     return string
 
 
-# Funcion para consultar la EMT
-def query_emt(parada):
+# Query EMT VLC and build msg
+def query_emt(data):
     # Mensaje = logs_size_str
-    emt_info = (subprocess.getoutput("pyemtvlc " + parada))
+    emt_info = (subprocess.getoutput("pyemtvlc " + data))
     return emt_info
